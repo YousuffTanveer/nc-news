e324e8c55373d266ec88a3b0777d793a2549e6b0
Yousuff <yousufftanveer77@gmail.com>
2023-01-13 11:58:44 +0000

    minor file cahnges

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Nav from "./Components/Nav";
 import Articles from "./Components/Articles";
@@ -9,13 +9,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Nav /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Articles />} />
-          {/* <Route path="/article/:article_id" element={<SinlgeArticle />} /> */}
-        </Routes>
-      </BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Articles />} />
+        <Route path="/:articleId" element={<SinlgeArticle />} />
+        <Route path="/topic/:topic_slug" element={<Articles />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,14 +1,12 @@
 export default function ArticleCard({ article }) {
   return (
-    <div className="Article-Card">
-      <article>
-        <h3 className="Article-Card-Header">{article.title}</h3>
-        <p>author: {article.author}</p>
-        <p>topic: {article.topic}</p>
-        <p>comment_count: {article.comment_count}</p>
-        <p>votes: {article.votes}</p>
-        <p>created at: {article.created_at}</p>
-      </article>
-    </div>
+    <article className="Article-Card" key={article.article_id}>
+      <h3 className="Article-Card-Header">{article.title}</h3>
+      <p>author: {article.author}</p>
+      <p>topic: {article.topic}</p>
+      <p>comment_count: {article.comment_count}</p>
+      <p>votes: {article.votes}</p>
+      <p>created at: {article.created_at}</p>
+    </article>
   );
 }
